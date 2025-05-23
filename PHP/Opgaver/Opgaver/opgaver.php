83c56295f7bc97d8601010790ebbfe770ccd24dc
AspIT-61752 <61752@edu.campusvejle.dk>
2025-05-23 11:34:42 +0200

    Opg 5 Done

--- PHP/Opgaver/Opgaver/opgaver.php
+++ PHP/Opgaver/Opgaver/opgaver.php
@@ -1,4 +1,42 @@
 <!-- https://tobi61752.aspitcloud.dk/V31/opgaver.php -->
+
+<?php
+// Opg 5
+function NameInClass($find){
+    $find = strtolower($find);
+    $arr = array("thomas", "kenneth", "mads", "simon");
+    for ($i=0; $i < count($arr); $i++) { 
+        if ($arr[$i] == $find) {
+            return $i;
+        }
+    }
+    return -1;
+}
+
+function NameInClassArr($find) {
+    $find = strtolower($find);
+    $arr = array("thomas", "kenneth", "mads", "simon");
+    $index = array_search($find, $arr);
+    return $index !== false ? $index : -1;
+}
+
+function populateArrayWMonths() {
+    $months = [];
+    $dt = new DateTime();
+    $dt->setDate(2025, 1, 1);
+
+    for ($i = 0; $i < 12; $i++) {
+        $monthName = $dt->format('F');
+        $daysInMonth = $dt->format('t');
+        $months[$monthName] = (int)$daysInMonth;
+        $dt->modify('+1 month');
+    }
+
+    return $months;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,7 +112,60 @@
     echo '<br>';
     echo "Is $word a palindrome: $awnser";
     ?>
+
+    <?php
+    echo '<br>';
+    $toFind = "kenneth";
+    $index = NameInClass($toFind);
+    $toFindCap = ucfirst(strtolower($toFind));
+    if ($index != -1) {
+        echo "$toFindCap is in the class at index $index";
+    } else {
+        echo "$toFindCap is not in the class";
+    }
+    ?>
+
+    <?php
+    echo '<br>';
+    $toFind = "thomas";
+    $index = NameInClassArr($toFind);
+    $toFindCap = ucfirst(strtolower($toFind));
+    if ($index != -1) {
+        echo "$toFindCap is in the class at index $index";
+    } else {
+        echo "$toFindCap is not in the class";
+    }
+    ?>
     
+    <?php
+    // Get months and their days
+    $months = populateArrayWMonths();
+
+    $shortMonths = [];
+    $longMonths = [];
+
+    foreach ($months as $month => $days) {
+        if ($days <= 30) {
+            $shortMonths[$month] = $days;
+        } elseif ($days == 31) {
+            $longMonths[$month] = $days;
+        }
+    }
+    ?>
+
+    <h3>Short Months (30 days or less):</h3>
+    <ul>
+        <?php foreach ($shortMonths as $month => $days): ?>
+            <li><?php echo htmlspecialchars($month); ?>: <?php echo $days; ?> days</li>
+        <?php endforeach; ?>
+    </ul>
+
+    <h3>Long Months (31 days):</h3>
+    <ul>
+        <?php foreach ($longMonths as $month => $days): ?>
+            <li><?php echo htmlspecialchars($month); ?>: <?php echo $days; ?> days</li>
+        <?php endforeach; ?>
+    </ul>
     
 </body>
 </html>
